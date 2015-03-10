8c490f1459e37fdcee4854d97c09413a30ac1079
ccwu <ccwu660601@mapacode.tw>
2015-03-10 20:18:33 +0800

    CCWU: get title from ApplicationStore.

--- examples/components/Html.jsx
+++ examples/components/Html.jsx
@@ -1,13 +1,17 @@
 'use strict';
 
 var React = require('react');
+var ApplicationStore = require('../stores/ApplicationStore');
+var FluxibleMixin = require('fluxible').Mixin;
+
 var Html = React.createClass({
+    mixins: [FluxibleMixin],
     render: function() {
         return (
             <html>
             <head>
                 <meta charSet="utf-8" />
-                <title>Botnana Blog View Examples</title>
+                <title>{this.getStore(ApplicationStore).getPageTitle()}</title>
                 <meta name="viewport" content="width=device-width, user-scalable=no" />
                 <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css" />
             </head>
