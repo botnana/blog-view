7d579ed440897f0c36a28f41a66c9c19b658ad30
ccwu <ccwu660601@mapacode.tw>
2015-03-12 18:36:15 +0800

    CCWU: added editor.css.

--- examples/components/Html.jsx
+++ examples/components/Html.jsx
@@ -14,6 +14,7 @@ var Html = React.createClass({
                 <title>{this.getStore(ApplicationStore).getPageTitle()}</title>
                 <meta name="viewport" content="width=device-width, user-scalable=no" />
                 <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css" />
+                <link rel="stylesheet" href="/public/assets/editor.css" />
             </head>
             <script src="/public/js/ace-builds/src/ace.js"></script>
             <body>
